7ed8f24db9349902aa660264b9ae7ab818d645db
Mikhail Baranov <baranov.mv@gmail.com>
2017-02-21 01:51:17 +0300

    Fixes an incosistent name of a field in the RoomResponseEstimator

--- evaluation.py
+++ evaluation.py
@@ -35,7 +35,7 @@ sig_env = signal_env.SoundEnvirenment(ir, ir_fl.rate)
 
 in_sig = np.array([])
 in_sig = np.append(in_sig, sig_env.run(np.zeros(1024)))
-in_sig = np.append(in_sig, sig_env.run(estimator.test_pulse))
+in_sig = np.append(in_sig, sig_env.run(estimator.probe_pulse))
 in_sig = np.append(in_sig, sig_env.run(np.zeros(10240)))
 
 room_response = estimator.estimate(in_sig)
@@ -43,26 +43,40 @@ room_response = estimator.estimate(in_sig)
 #######################################################################################################################
 # Display
 
-delta = scipy.signal.fftconvolve( estimator.reverse_pulse, estimator.test_pulse)
+delta = scipy.signal.fftconvolve( estimator.reverse_pulse, estimator.probe_pulse)
 # room_response = room_response[:ir.shape[0]]
 ir = ir[:13400]
 offset = 1223
 error = ir-room_response[offset:13400+offset]
 
 plt.subplot(311)
-plt.plot(ir, 'b')
-plt.plot(room_response[offset:], 'r' )
+legends = []
+legend, = plt.plot(ir, 'b', label="True IR")
+legends.append(legend)
+legend, = plt.plot(room_response[offset:], 'r', label="Estimated IR" )
+legends.append(legend)
 plt.axis([-10,500, -1, 1])
 # plt.axis([0,2000,-1,1])
 plt.grid()
+plt.legend(handles=legends)
+
 plt.subplot(312)
-plt.plot(error)
+legends = []
+legend, = plt.plot(error, label="Absolute error")
+legends.append(legend)
 plt.grid()
+plt.legend(handles=legends)
 
 plt.subplot(313)
-plt.plot( *Spectrum(room_response) )
+legends = []
 xf,R = Spectrum(ir)
-plt.plot( xf, R )
+legend, = plt.plot( xf, R, label="Spectrum of the true IR" )
+legends.append(legend)
+
+exf, eR = Spectrum(room_response)
+legend, = plt.plot( exf, eR, label="Spectrum of the estimated IR" )
+legends.append(legend)
+plt.legend(handles=legends)
 plt.grid()
 
 plt.show()
